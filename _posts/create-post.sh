d6b0d1f6b5de468cd3ca7f84c7b11202a9f24d2c
Saki Homma <sakkuru0323@gmail.com>
2015-05-05 21:15:50 +0900

    modified create-post.sh

--- _posts/create-post.sh
+++ _posts/create-post.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-src_dir=..
-dst_dir=.
+src_dir=.
+dst_dir=_posts
 
-rm -rf $dst_dir/*
+#rm -rf $dst_dir/*
 cp $src_dir/*.md $dst_dir/
 cd $dst_dir
-ls -1  | xargs -I {} mv {} `date "+%Y-%m-%d-"{}`
+ls -1 *.md  | xargs -I {} mv {} `date "+%Y-%m-%d-"{}`
